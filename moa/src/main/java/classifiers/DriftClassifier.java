c54a772b0412346539a3fabee02a35e5e294d64e
Yifan Zhang <yifan127@gmail.com>
2018-06-04 15:01:21 +1200

    add a parameter for singleClassifierDrift
    add a parameter for singleClassifierDrift

--- moa/src/main/java/classifiers/DriftClassifier.java
+++ moa/src/main/java/classifiers/DriftClassifier.java
@@ -1,13 +1,26 @@
 package classifiers;
 
+
 import moa.classifiers.core.driftdetection.AbstractChangeDetector;
 import moa.classifiers.drift.SingleClassifierDrift;
+import moa.classifiers.drift.DriftDetectionMethodClassifier;
 import moa.classifiers.trees.HoeffdingTree;
 
 public class DriftClassifier {
 	
-	public SingleClassifierDrift getSingleClassifierDrift(AbstractChangeDetector detector){
+	public SingleClassifierDrift getSingleClassifierDrift(AbstractChangeDetector detector, long truePoint){
 		SingleClassifierDrift learner = new SingleClassifierDrift();
+		HoeffdingTree baseLearner = this.getHoeffdingTree();
+		learner.baseLearnerOption.setCurrentObject(baseLearner);
+		learner.driftDetectionMethodOption.setCurrentObject(detector);
+		learner.setTrueChangePoint(truePoint);
+		learner.prepareForUse();
+		
+		return learner;
+	}
+	
+	public DriftDetectionMethodClassifier getDDMClassifier(AbstractChangeDetector detector){
+		DriftDetectionMethodClassifier learner = new DriftDetectionMethodClassifier();
 		HoeffdingTree baseLearner = this.getHoeffdingTree();
 		learner.baseLearnerOption.setCurrentObject(baseLearner);
 		learner.driftDetectionMethodOption.setCurrentObject(detector);
